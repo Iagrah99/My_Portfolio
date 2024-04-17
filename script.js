3dbc6dbdc9296982345c297abef7b5afbee77591
Iagrah99 <i4ng1999@gmail.com>
2024-04-17 18:17:33 +0100

    fixes z-index on navbar on mobile view

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const heroContent = document.getElementsByClassName("hero-content")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
+  heroContent.classList.toggle("pushed");
 });
