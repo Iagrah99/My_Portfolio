6e800fb939792fbbf7f26593dcc5235eceb95356
Ian Graham <i4ng1999@gmail.com>
2024-08-23 00:40:24 +0100

    create skills.js file and use it to render HTML

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const heroContent = document.getElementsByClassName("hero-content")[0];
+import { skills } from './skills.js';
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-  heroContent.classList.toggle("pushed");
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const heroContent = document.getElementsByClassName('hero-content')[0];
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+  heroContent.classList.toggle('pushed');
 });
+
+function renderSkills() {
+  let skillsHTML = '';
+
+  skills.forEach((skill) => {
+    skillsHTML += `
+     <figure>
+      <img src="${skill.image}" width="512" height="512" alt="${skill.altText}" title="${skill.titleText}" />
+      <figcaption>${skill.name}</figcaption>
+    </figure>
+    `;
+  });
+
+  document.querySelector('.js-skill-icons').innerHTML = skillsHTML;
+}
+
+renderSkills();
---
+++ skills.js
@@ -0,0 +1,74 @@
+export const skills = [
+  {
+    name: 'HTML5',
+    image: './img/icons/HTML5-logo.png',
+    altText: 'HTML5 Logo',
+    titleText: 'HTML5',
+  },
+  {
+    name: 'CSS',
+    image: './img/icons/CSS-logo.png',
+    altText: 'CSS Logo',
+    titleText: 'CSS',
+  },
+  {
+    name: 'JavaScript',
+    image: './img/icons/JS-logo.png',
+    altText: 'JavaScript Logo',
+    titleText: 'JavaScript',
+  },
+  {
+    name: 'React Native',
+    image: './img/icons/React-Native-Logo.png',
+    altText: 'React Native Logo',
+    titleText: 'React Native',
+  },
+  {
+    name: 'PostGreSQL',
+    image: './img/icons/postgresql-icon.png',
+    altText: 'PostGreSQL Logo',
+    titleText: 'PostGreSQL',
+  },
+  {
+    name: 'Express JS',
+    image: './img/icons/Express_Logo.png',
+    altText: 'Express JS Logo',
+    titleText: 'Express JS',
+  },
+  {
+    name: 'React',
+    image: './img/icons/React-logo.png',
+    altText: 'React Logo',
+    titleText: 'React',
+  },
+  {
+    name: 'Node JS',
+    image: './img/icons/Node-Js-Logo.png',
+    altText: 'Node JS Logo',
+    titleText: 'Node JS',
+  },
+  {
+    name: 'Git',
+    image: './img/icons/Git-Logo.png',
+    altText: 'Git Logo',
+    titleText: 'Git',
+  },
+  {
+    name: 'Responsive Design',
+    image: './img/icons/Responsive-Design-Icon.png',
+    altText: 'Responsive Design Image',
+    titleText: 'Responsive Design',
+  },
+  {
+    name: 'Testing Logo',
+    image: './img/icons/Testing-logo.png',
+    altText: 'Testing Logo Image',
+    titleText: 'Testing Logo',
+  },
+  {
+    name: 'Accessibility Logo',
+    image: './img/icons/Accessibility_logo.png',
+    altText: 'Accessibility Logo Image',
+    titleText: 'Accessibility Logo',
+  },
+];
