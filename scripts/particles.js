54e7eb94a88369268cdae6351aee973ca9cb1a32
Iagrah99 <i4ng1999@gmail.com>
2025-04-22 15:17:08 +0100

    Decrease particle speed upon collision

--- scripts/particles.js
+++ scripts/particles.js
@@ -50,17 +50,17 @@ class Particle {
     let dy = mouse.y - this.y;
     let distance = Math.sqrt(dx * dx + dy * dy);
     if (distance < mouse.radius + this.size) {
-      if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
-        this.x += 10;
+      if (mouse.x < this.x && this.x < canvas.width - this.size * 3) {
+        this.x += 3;
       }
-      if (mouse.x > this.x && this.x > this.size * 10) {
-        this.x -= 10;
+      if (mouse.x > this.x && this.x > this.size * 3) {
+        this.x -= 3;
       }
-      if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
-        this.y += 10;
+      if (mouse.y < this.y && this.y < canvas.height - this.size * 3) {
+        this.y += 3;
       }
-      if (mouse.y > this.y && this.y > this.size * 10) {
-        this.y -= 10;
+      if (mouse.y > this.y && this.y > this.size * 3) {
+        this.y -= 3;
       }
     }
     // move particle
